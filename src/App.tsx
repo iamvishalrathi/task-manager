214eed225e0cc648b97761ba30102ff4b0a550b6
Your Name <rajatrathi029@gmail.com>
2025-09-11 03:23:18 +0530

    feat: update meta tags in index.html for improved SEO and change Toaster position in App component

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
         <div className="App">
           <AuthChecker />
           <Toaster
-            position="top-right"
+            position="bottom-right"
             toastOptions={{
               duration: 4000,
               style: {
